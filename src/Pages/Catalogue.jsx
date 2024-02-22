ba74ce66e6ffe8d024deba758848d7884b43352c
abhisheksooni <abhisheksoni.as444@gmail.com>
2024-02-22 23:56:17 +0530

    Fix: set loading in cataogue section

--- src/Pages/Catalogue.jsx
+++ src/Pages/Catalogue.jsx
@@ -12,46 +12,59 @@ function addItem(i){
 dispatch(AddCartItem(i))
 }
 
-const [showLoading,SetLoading] = useState(false)
-
+const [showLoading,SetLoading] = useState(true)
+const [Data,SetDAta] = useState(null)
 
 useEffect(()=>{
+const d = async ()=>{
 
-  const timer = setTimeout(()=>SetLoading(true),300)
+    try{
+      const rep = await data  
+      return SetDAta(rep)
+    } catch{
+      console.error(error);
+    } finally{
+      SetLoading(false)
+    }
+}
+d()
 
- window.scroll(0,0)
+  window.scroll(0,0)
 
-  return ()=> clearTimeout(timer)
-},[])
+  // const timer = setTimeout(()=>SetLoading(true),2000)
+  // return ()=> clearTimeout(timer)
+},[Data])
 
   return (
     <>
-    <div className=" h-[100vh]">
-
-   
+    {/* <div className=" h-[100vh]">
     {
-      showLoading && (
-        <section className=' flex flex-wrap  justify-center mx-auto max-w-[1200px] gap-6'>
-      
-        {
-          data.map((i)=>(
-            <div  key={i.id} >
-              <Link to={`/prodect/${i.id}`}>
-        <ItemCard img={i.thumbnail} name={i.title} />
-              </Link>
-              <div className=" flex  items-center px-2 justify-between ">
-                <p className=' text-c5'> <span className='text-c4'>{i.sale? i.sale: ""}</span> ${sale?<del>800</del>:i.price}</p>
-                  <button onClick={()=>addItem(i)} className='  rounded-full p-2 bg-c2 text-c1'><ShoppingCart size={20}/></button>
-                </div>
-            </div>
-          ))
-        }
-        
-        
-            </section>
-      )
+      showLoading && (   )
     }
-   </div>
+   </div> */}
+   {
+    showLoading?(<div className=' h-[100vh]'>
+      <p className='text-9xl'>Loding........</p>
+    </div>):(
+      <section className=' flex flex-wrap  justify-center mx-auto max-w-[1200px] gap-6'>
+         
+         {
+           Data.map((i)=>(
+             <div  key={i.id} >
+               <Link to={`/prodect/${i.id}`}>
+         <ItemCard img={i.thumbnail} name={i.title} />
+               </Link>
+               <div className=" flex  items-center px-2 justify-between ">
+                 <p className=' text-c5'> <span className='text-c4'>{i.sale? i.sale: ""}</span> ${sale?<del>800</del>:i.price}</p>
+                   <button onClick={()=>addItem(i)} className='  rounded-full p-2 bg-c2 text-c1'><ShoppingCart size={20}/></button>
+                 </div>
+             </div>
+           ))
+         }
+         
+         
+             </section>)
+   }
   </>
   )
 }
